a9e0dd274e146fcc0012d8960ba2add1342d0ea3
Alex Faria <alex.faria@me.com>
2018-05-07 10:21:16 +0100

    pre-reuniao commit

--- src/pages/camara/camara.ts
+++ src/pages/camara/camara.ts
@@ -21,7 +21,7 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 
 
 export class CamaraPage {
-  myphoto: any;
+  base64Image: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
   }
@@ -37,7 +37,7 @@ export class CamaraPage {
     this.camera.getPicture(options).then((imageData) => {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64:
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
     }, (err) => {
       // Handle error
     });
